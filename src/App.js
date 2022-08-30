5057bfa995000552910e0ef42f5572d3a92bf94d
rebeccalpeace <rebeccalpeace@gmail.com>
2022-08-30 09:26:59 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [things, setThings] = useState([]);
+
+	function handleAddClick(e){
+		e.preventDefault();
+		const toDo = e.target.add.value;
+		let allThings = [...things, toDo]
+		setThings(allThings)
+	}
+
+	function handleDoneClick(e){
+		const done = e.target.remove.value;
+		let allThings = things
+		for (let i=0; i < allThings.length; i++){
+			if (allThings[i] === done){
+				allThings.splice(i, 1);
+			} 
+		}
+		setThings(allThings)
+	}
+
+	return (
+		<>
+			<Navbar />
+			<div className='container'>
+				<div className="row mt-5 me-2 ms-2 d-flex justify-content-around">
+					<form onSubmit={handleAddClick} className="col-6 d-flex ">
+						<label htmlFor="add" className="form-label me-2">Add:</label>
+						<input type='text' className='form-control me-2' name='add' />
+						<input type='submit' value='Add' className='btn btn-success' />
+					</form>
+					<form className="col-6">
+						<div className="input-group">
+							<label htmlFor="remove" className="form-label me-2">Done with:</label>
+							<select className="form-select" onSubmit={handleDoneClick} id="inputGroupSelect04" name="remove" aria-label="Example select with button addon">
+								<option defaultValue="Choose...">Choose...</option>
+								{things.map((t, i) => <option value={i+1}>{t}</option>)}
+							</select>
+							<input className="btn btn-success" type="submit" value="Done" />
+						</div>
+					</form>
+				</div>
+				<table className='table mt-5 w-25 ms-auto me-auto'>
+					<thead>
+						<tr>
+							<th scope="col"></th>
+							<th scope="col"></th>
+						</tr>
+					</thead>
+					<tbody>
+						{things.map((t, i) => <tr><th scope='row'>{i+1}</th><td>{t}</td></tr>)}
+					</tbody>
+				</table>
+			</div>
+		</>
+	)
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-md">
+                <a className="navbar-brand" href="/">To-Do List</a>
+                <a className="navbar-brand fs-6" href="/">Clear List</a>
+            </div>
+        </nav>
+    )
+}
